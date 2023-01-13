cd4fcd385593e2158fdcc48a0b272ba36c61fa0a
queueOYL <101844882+queueOYL@users.noreply.github.com>
2023-01-13 15:16:35 +0800

    Update Dense.py

--- Dense.py
+++ Dense.py
@@ -1,25 +1,35 @@
 import tensorflow as tf
-
+import numpy as np
+from tensorflow.keras.utils import to_categorical
 mnist=tf.keras.datasets.mnist
+
 (x_train,t_train),(x_test,t_test)=mnist.load_data()#读取数据
-x_train,x_test = x_train/255.0 , x_test/255.0 #初始化图像的256个色度于0—1区间上
+t_train = to_categorical(t_train,10)
+t_test = to_categorical(t_test,10)
+x_train = x_train.astype(np.float32)
+x_test = x_test.astype(np.float32)
+x_train = x_train.reshape([-1,28,28,1])
+x_test = x_test.reshape([-1,28,28,1])
+
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28,28)),
-    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
     tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
+    tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(10,activation='softmax')
 ])
-     
+
 model.compile(
-    optimizer='adam',
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),#损失函数
-    metrics=['accuracy']
+    optimizer="sgd",
+    loss="categorical_crossentropy",
+    metrics="acc"
 )
 
-model.fit(x_train, t_train, batch_size=60, epochs=10, validation_data=(x_test,t_test), validation_freq=2)
+model.fit(x_train, t_train, batch_size=60, epochs=10, validation_data=(x_test,t_test), validation_freq=5)
 model.summary()
 
-
-
+model.save('DenseModel.h5')
